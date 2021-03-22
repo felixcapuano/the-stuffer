2af7dcf92e3c0e7d5efb4a96ce96654931035c45
MammothOG <felix.capuano@gmail.com>
2021-03-22 20:14:44 +0100

    stc schema

--- the-stuffer-container/app/validation/stuff/throwing-schema.js
+++ the-stuffer-container/app/validation/stuff/throwing-schema.js
@@ -1,5 +1,47 @@
 exports.stcSchema = {
-
+    "hit_id": {
+        isString: true,
+    },
+    "lat": {
+        isFloat: true,
+    },
+    "lng": {
+        isFloat: true,
+    },
+    "type": {
+        isString: true,
+        isIn: ["throw", "jumpthrow", "runjumpthrow"]
+    },
+    "yt_id": {
+        isString: true,
+        isLength: {
+            options: {
+                min: 11,
+                max: 11
+            }
+        }
+    },
+    "yt_start_time": {
+        isInt: {
+            options: {
+                min: 0,
+            }
+        },
+    },
+    "ticks64": {
+        isBoolean: true,
+    },
+    "ticks128": {
+        isBoolean: true,
+    },
+    "description": {
+        isString: true,
+        isLength: {
+            options: {
+                max: 255
+            }
+        }
+    }
 };
 
 exports.stdSchema = {
