deba94077f365cfa7172aedbb7953ab9fe801890
felixcapuano <felix.capuano@gmail.com>
2021-04-06 16:32:54 +0200

    adding user

--- mongo/init-scripts/user.js
+++ mongo/init-scripts/user.js
@@ -1,5 +1,16 @@
 db.createUser({
-    user    : "backend",
+    user    : "stuff",
+    pwd     : "123",
+    roles   : [
+        {
+            role : "readWrite",
+            db   : "thestuffer"
+        }
+    ]
+})
+
+db.createUser({
+    user    : "auth",
     pwd     : "123",
     roles   : [
         {
