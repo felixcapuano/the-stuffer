73775af4fb300eb3c036b1dc406847ff4d1c08c2
felixcapuano <felix.capuano@gmail.com>
2021-04-12 14:07:21 +0200

    finish throwing layer

--- thestuffer/app/src/components/_Map/ThrowingLayer.jsx
+++ thestuffer/app/src/components/_Map/ThrowingLayer.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 
 import StuffDataContext from '../../context/StuffDataContext';
@@ -6,19 +6,24 @@ import StuffDataContext from '../../context/StuffDataContext';
 import L from 'leaflet';
 import 'leaflet.heat';
 
+const throwingLayer = L.layerGroup();
+
 const ThrowingLayer = ({ target }) => {
   const { getThrowingData } = useContext(StuffDataContext);
 
-  // TODO !!!!!!!!
-  // createt state/ref to save heatmap when re rendering
-  // the component
   const throwingData = getThrowingData(target);
-  const data = throwingData.map((d) => [d.lat, d.lng, 100]);
+  const data = throwingData.map((d) => [d.lat, d.lng, 1]);
 
   let heatmap = L.heatLayer(data, { radius: 25 });
 
   const map = useMap();
-  map.addLayer(heatmap);
+  if (!map.hasLayer(throwingLayer)) map.addLayer(throwingLayer);
+
+  throwingLayer.addLayer(heatmap);
+
+  useEffect(() => {
+    return () => throwingLayer.clearLayers();
+  });
 
   return null;
 };
