e291f961c2c019ddd55cbe8d93f444cdfbd930a6
felixcapuano <felix.capuano@gmail.com>
2021-04-13 12:56:03 +0200

    create stuff : schema validation fixing

--- stuff/app/validation/validation.js
+++ stuff/app/validation/validation.js
@@ -1,5 +1,5 @@
 const Ajv = require('ajv');
-const ajv = new Ajv({useDefaults: true, allErrors: true});
+const ajv = new Ajv({ useDefaults: true, allErrors: true });
 
 const { isIdExist } = require('../mongo/core');
 const throwingSchema = require('./throwing-schema');
@@ -10,27 +10,26 @@ ajv.addKeyword({
   async: true,
   type: 'string',
   validate: async (schema, id, type, { parentData }) => {
-    const idExist = await isIdExist(parentData.collection, id);
+    const idExist = await isIdExist('landing', id);
     return idExist;
-  }
+  },
 });
 
 const schema = {
-  'throwing': {
-    'create': ajv.compile(throwingSchema.create),
-    'update': ajv.compile(throwingSchema.update),
-    'search': ajv.compile(throwingSchema.search),
+  throwing: {
+    create: ajv.compile(throwingSchema.create),
+    update: ajv.compile(throwingSchema.update),
+    search: ajv.compile(throwingSchema.search),
   },
-  'landing': {
-    'create': ajv.compile(landingSchema.create),
-    'update': ajv.compile(landingSchema.update),
-    'search': ajv.compile(landingSchema.search),
-  }
-}
+  landing: {
+    create: ajv.compile(landingSchema.create),
+    update: ajv.compile(landingSchema.update),
+    search: ajv.compile(landingSchema.search),
+  },
+};
 
 module.exports = (_method) => {
   return async (req, res, next) => {
-
     const validate = schema[req.body.collection]?.[_method];
     if (!validate) return res.sendStatus(404);
 
@@ -38,8 +37,12 @@ module.exports = (_method) => {
       const data = await validate(req.body);
     } catch (error) {
       console.error(error.errors);
-      return res.send({ ok: false, message: 'Bad format', errors: error.errors});
+      return res.send({
+        ok: false,
+        message: 'Bad format',
+        errors: error.errors,
+      });
     }
     next();
-  }
-}
+  };
+};
--- thestuffer/app/src/components/Map/fill_db.js
+++ thestuffer/app/src/components/Map/fill_db.js
@@ -1,32 +1,37 @@
 import { stuffInstance } from '../../axios';
 
-const HIT_LEN = 10;
-const THROW_LEN = 100;
+const HIT_LEN = 1;
+const THROW_LEN = 1;
 
 const throwingFiller = (rep) => {
   for (let i = 0; i < THROW_LEN; i++) {
     const throwData = {
-      hit_id: rep.data._id,
-      lat: parseFloat((Math.random() * 180 - 90).toFixed(2)),
-      lng: parseFloat((Math.random() * 360 - 180).toFixed(2)),
-      type: 'jumpthrow',
-      yt_id: 'PtxMYGxQ9Zc',
-      yt_start_time: 133,
-      ticks64: true,
-      ticks128: true,
+      collection: 'throwing',
+      landing_id: rep.data._id,
+      position: {
+        lat: parseFloat((Math.random() * 180 - 90).toFixed(2)),
+        lng: parseFloat((Math.random() * 360 - 180).toFixed(2)),
+        floor: 0,
+      },
+      movement: 'jumpthrow',
+      video: {
+        id: 'PtxMYGxQ9Zc',
+        time: 133,
+      },
+      tickrate: {
+        64: true,
+        128: true,
+      },
       description: 'je suis une description',
-      validated: false,
-      likes: 0,
-      dislikes: 0,
-      reported: [],
-      count: 1,
     };
+
+    stuffInstance.post('/stuff/create', throwData).then((rep) => {
+      console.log(rep.data);
+    });
   }
 };
 
 const filler = () => {
-  let landingsList = [];
-
   for (let i = 0; i < HIT_LEN; i++) {
     const landData = {
       collection: 'landing',
@@ -40,6 +45,7 @@ const filler = () => {
     };
     stuffInstance.post('/stuff/create', landData).then((rep) => {
       throwingFiller(rep);
+      console.log(rep.data._id);
     });
   }
 };
