c24e091a6bfa846830b2aecce520ee3fb3b10da1
felixcapuano <felix.capuano@gmail.com>
2021-04-14 19:58:41 +0200

    tiles fixing

--- thestuffer/app/src/components/Map/TilesLayer.jsx
+++ thestuffer/app/src/components/Map/TilesLayer.jsx
@@ -1,27 +1,41 @@
-import L from 'leaflet';
-import { LayersControl, useMap } from 'react-leaflet';
+import L, { tileLayer } from 'leaflet';
+import { useEffect, useRef, useState } from 'react';
+import { LayersControl, useMap, TileLayer } from 'react-leaflet';
 
 const tilesLayerGroup = L.layerGroup();
-const url = window.location.origin + '/images/maps/{map}/{z}/{y}/{x}.png';
 
 const TilesLayer = ({ mapName }) => {
-  const map = useMap();
-  if (!map.hasLayer(tilesLayerGroup)) map.addLayer(tilesLayerGroup);
+  // const map = useMap();
+  // if (!map.hasLayer(tilesLayerGroup)) map.addLayer(tilesLayerGroup);
 
-  const newTilesLayer = L.tileLayer(url, {
-    map: mapName,
+  // const newTilesLayer = L.tileLayer();
+  useState(mapName);
+
+  const tileLayer = useRef();
+  const tilesUrl = (m) =>
+    window.location.origin + '/images/maps/' + m + '/{z}/{y}/{x}.png';
+
+  // tilesLayerGroup.clearLayers();
+  // tilesLayerGroup.addLayer(newTilesLayer);
+  useEffect(() => {
+    tileLayer.current.setUrl(tilesUrl(mapName), false);
+  });
+
+  //console.log(url('de_inferno'));
+  const tileLayerConfig = {
+    url: tilesUrl(mapName),
     minZoom: 0,
     maxZoom: 3,
     noWrap: true,
-  });
-
-  tilesLayerGroup.clearLayers();
-  tilesLayerGroup.addLayer(newTilesLayer);
+    ref: tileLayer,
+  };
 
   // TODO watch for react leaflet Tile Layer to replace with useRef
   return (
     <LayersControl position='topleft' collapsed={false}>
-      <LayersControl.BaseLayer name={'1'}></LayersControl.BaseLayer>
+      <LayersControl.BaseLayer checked name='0'>
+        <TileLayer {...tileLayerConfig} />
+      </LayersControl.BaseLayer>
     </LayersControl>
   );
 };
