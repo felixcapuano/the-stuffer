c90d4e210b7fc184e27385b13dfb9969b7b7dea9
felixcapuano <felix.capuano@gmail.com>
2021-04-17 15:04:13 +0200

    card

--- thestuffer/app/src/components/ThrowingCard/ThrowingCard.jsx
+++ thestuffer/app/src/components/ThrowingCard/ThrowingCard.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 import Badge from 'react-bootstrap/Badge';
+import Button from 'react-bootstrap/Button';
 
 import './ThrowingCard.css';
 
 const ThrowingCard = ({ data }) => {
+  const [open, setOpen] = useState(false);
+
+  const ytUrl = `https://www.youtube.com/embed/${data.video.id}?start=${data.video.time}`;
+
   const tickrateBadge = (tickrate) => (
     <Badge className='tickBadge' variant='dark'>
       {tickrate}
@@ -17,20 +22,36 @@ const ThrowingCard = ({ data }) => {
       jumpthrow: 'warning',
       runjumpthrow: 'danger',
     };
-    return (<Badge variant={colorBinding[movement]}>
-        {movement}
-    </Badge>)
+    return <Badge variant={colorBinding[movement]}>{movement}</Badge>;
   };
 
   return (
     <Card className='throwingCard'>
-      <div>
+      <Card.Header>
         {data.tickrate['64'] && tickrateBadge('64')}
         {data.tickrate['128'] && tickrateBadge('128')}
         {movementBadge(data.movement)}
-      </div>
+      </Card.Header>
       <Card.Body>
         <Card.Text>Description : {data.description}</Card.Text>
+        <Button
+          className='openButton'
+          onClick={() => setOpen(!open)}
+          variant='dark'
+        >
+          {open ? '^' : 'Video'}
+        </Button>
+        {open && (
+          <iframe
+            width='100%'
+            height='500'
+            src={ytUrl}
+            title='YouTube video player'
+            frameBorder='0'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allowFullScreen
+          />
+        )}
       </Card.Body>
     </Card>
   );
