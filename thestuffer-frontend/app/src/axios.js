4112fe537991aec767b98cc993058a5cc99f0c75
felixcapuano <felix.capuano@gmail.com>
2021-04-25 22:51:46 +0200

    nginx

--- thestuffer-frontend/app/src/axios.js
+++ thestuffer-frontend/app/src/axios.js
@@ -8,20 +8,21 @@ const addAuthHeader = (data, headers) => {
 
 const AUTH_HOST = process.env.REACT_APP_AUTH_HOST;
 const AUTH_PORT = process.env.REACT_APP_AUTH_PORT;
-console.log(AUTH_HOST + ' ' + AUTH_PORT);
 
+console.log(window.location)
 export const authInstance = axios.create({
-  baseURL: `http://${AUTH_HOST}:${AUTH_PORT}/`,
+  // baseURL: 'http://localhost:3002/',
+  baseURL: window.location.origin + '/auth',
   withCredentials: true,
   transformRequest: [addAuthHeader, ...axios.defaults.transformRequest],
 });
 
 const STUFF_HOST = process.env.REACT_APP_STUFF_HOST;
 const STUFF_PORT = process.env.REACT_APP_STUFF_PORT;
-console.log(STUFF_HOST + ' ' + STUFF_PORT);
 
 export const stuffInstance = axios.create({
-  baseURL: `http://${STUFF_HOST}:${STUFF_PORT}/`,
+  // baseURL: `http://${STUFF_HOST}:${STUFF_PORT}/`,
+  baseURL: window.location.origin + '/ressources',
   withCredentials: true,
   transformRequest: [addAuthHeader, ...axios.defaults.transformRequest],
 });
