96e59e48ddec74527235beca946efe6abffc3735
felixcapuano <felix.capuano@gmail.com>
2021-04-27 14:13:04 +0200

    fixing env variable typo

--- thestuffer-frontend/app/src/axios.js
+++ thestuffer-frontend/app/src/axios.js
@@ -6,8 +6,8 @@ const addAuthHeader = (data, headers) => {
   return data;
 };
 
-const AUTH_HOST = process.env.REACT_APP_AUTH_HOST;
-const AUTH_PORT = process.env.REACT_APP_AUTH_PORT;
+// const AUTH_HOST = process.env.REACT_APP_AUTH_HOST;
+// const AUTH_PORT = process.env.REACT_APP_AUTH_PORT;
 
 console.log(window.location)
 export const authInstance = axios.create({
@@ -17,8 +17,8 @@ export const authInstance = axios.create({
   transformRequest: [addAuthHeader, ...axios.defaults.transformRequest],
 });
 
-const STUFF_HOST = process.env.REACT_APP_STUFF_HOST;
-const STUFF_PORT = process.env.REACT_APP_STUFF_PORT;
+// const STUFF_HOST = process.env.REACT_APP_STUFF_HOST;
+// const STUFF_PORT = process.env.REACT_APP_STUFF_PORT;
 
 export const stuffInstance = axios.create({
   // baseURL: `http://${STUFF_HOST}:${STUFF_PORT}/`,
