3396e54a55f4806271e65eaba8a8d88126b8f0bf
felixcapuano <felix.capuano@gmail.com>
2021-05-12 12:21:27 +0200

    final

--- mongo/db_setup.sh
+++
@@ -1,7 +0,0 @@
-mongo ${MONGO_INITDB_DATABASE} \
---host localhost \
---port ${MONGO_PORT} \
--u ${MONGO_INITDB_ROOT_USERNAME} \
--p ${MONGO_INITDB_ROOT_PASSWORD} \
---authenticationDatabase admin \
---eval "db.createUser({user: '${STUFF_MONGO_USERNAME}', pwd: '${STUFF_MONGO_PASSWORD}', roles:[{role:'readWrite', db: '${MONGO_INITDB_DATABASE}'}]});db.createUser({user: '${AUTH_MONGO_USERNAME}', pwd: '${AUTH_MONGO_PASSWORD}', roles:[{role:'readWrite', db: '${MONGO_INITDB_DATABASE}'}]});"
