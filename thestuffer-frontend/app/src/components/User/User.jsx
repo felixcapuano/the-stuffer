94951744d911b76e77bbbe7331f8102522d68e6f
felixcapuano <felix.capuano@gmail.com>
2021-05-17 17:57:31 +0200

    adding user profiles

--- thestuffer-frontend/app/src/components/User/User.jsx
+++ thestuffer-frontend/app/src/components/User/User.jsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 
 import { authInstance } from '../../axios';
 
@@ -11,7 +16,7 @@ const User = () => {
   const [username, setUsername] = useState('undefined');
   const [creation, setCreation] = useState('undefined');
   const [email, setEmail] = useState('undefined');
-  const [verifed, setVerified] = useState(false);
+  const [verified, setVerified] = useState(false);
   const [role, setRole] = useState('undefined');
 
   useEffect(() => {
@@ -26,16 +31,58 @@ const User = () => {
   }, [setUsername, setCreation, setEmail, setVerified, setRole, history]);
 
   return (
-    <ul>
-      <li>{creation}</li>
-      <li>{role}</li>
+    <Col xl={{ span: 4, offset: 4 }}>
+      <Form className='profile'>
+        <img src='/images/icons/profileIcon.png' className='profileIcon' />
+        <InputGroup className='profileInput'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Role</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control value={role} readOnly />
+        </InputGroup>
+        <InputGroup className='profileInput'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Username</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control value={username} readOnly />
+        </InputGroup>
+        <InputGroup className='profileInput'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Email</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control value={email} readOnly />
+        </InputGroup>
+        <InputGroup className='profileInput'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Password</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control value='************' readOnly />
+          <InputGroup.Append>
+            <Button variant='dark'>Change password</Button>
+          </InputGroup.Append>
+        </InputGroup>
+        <InputGroup className='profileInput'>
+          <InputGroup.Prepend>
+            <InputGroup.Text>Verified</InputGroup.Text>
+          </InputGroup.Prepend>
+          <Form.Control value={verified.toString()} readOnly />
+          {!verified && (
+            <InputGroup.Append>
+              <InputGroup.Text>
+                <Link to='/user/verification'>(verify email)</Link>
+              </InputGroup.Text>
+            </InputGroup.Append>
+          )}
+        </InputGroup>
+        {/* <li>{role}</li>
       <li>{email}</li>
       <li>{username}</li>
       <li>
         {verifed.toString()}
         {!verifed && <Link to='/user/verification'>(verify email)</Link>}
-      </li>
-    </ul>
+      </li> */}
+      </Form>
+    </Col>
   );
 };
 
